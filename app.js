6d1e993343a5eefdf5208f34655dbaa09cb54358
CJLynch01 <cjlynch@byui.edu>
2024-10-12 16:40:47 -0600

    Save

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  .use(bodyParser.json())
+  .use(express.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
